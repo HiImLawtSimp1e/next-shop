9fff2464c86758e9eb0f2bc3aa8b0b786a9dafa5
HiImLawtSimp1e <nqh1703k2@gmail.com>
2024-05-23 13:21:27 +0700

    Pre-Commit(Update FakeAPI)

--- next-shop-fakeapi/server.js
+++ next-shop-fakeapi/server.js
@@ -2,13 +2,13 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const db = router.db;
 
 // Using default middleware
 server.use(middlewares);
 
 // [GET] /products-by-category?slug=
 server.get("/products-by-category", (req, res) => {
-  const db = router.db;
   const { slug } = req.query;
 
   if (!slug) {
@@ -27,7 +27,80 @@ server.get("/products-by-category", (req, res) => {
     .get("products")
     .filter({ categoryId: category.id })
     .value();
-  res.json(products);
+
+  // get images of product
+  const enrichedPosts = products.map((product) => {
+    const image = db
+      .get("images")
+      .find({ productId: product.id, isMain: true, isSub: false })
+      .value();
+
+    const media_image = db
+      .get("images")
+      .find({ productId: product.id, isMain: false, isSub: true })
+      .value();
+
+    return {
+      ...product,
+      image: image.image,
+      media_image: media_image.image,
+    };
+  });
+  res.json(enrichedPosts);
+});
+
+// [GET] /product-detail?slug=
+server.get("/product-detail", (req, res) => {
+  const { slug } = req.query;
+
+  if (!slug) {
+    return res.status(400).send({ error: "Product slug is required" });
+  }
+
+  // get category by slug
+  const product = db.get("products").find({ slug }).value();
+
+  if (!product) {
+    return res.status(404).send({ error: "Product not found" });
+  }
+
+  // get products list by categoryId
+  const images = db.get("images").filter({ productId: product.id }).value();
+  const result = { ...product, images };
+  console.log(result);
+  res.json(result);
+});
+
+// [GET] /list-product
+server.get("/list-product", (req, res) => {
+  // get product list
+  let products = db.get("products").value();
+  if (!products) {
+    return res.status(404).send({ error: "Product not found" });
+  }
+  // Limit the number of products
+  const limit = 8; // Change this to your desired limit
+  products = products.slice(0, limit);
+
+  // get images of product
+  const enrichedPosts = products.map((product) => {
+    const image = db
+      .get("images")
+      .find({ productId: product.id, isMain: true, isSub: false })
+      .value();
+
+    const media_image = db
+      .get("images")
+      .find({ productId: product.id, isMain: false, isSub: true })
+      .value();
+
+    return {
+      ...product,
+      image: image.image,
+      media_image: media_image.image,
+    };
+  });
+  res.json(enrichedPosts);
 });
 
 server.use(router);
